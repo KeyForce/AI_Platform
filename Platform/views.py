210262ef1f58130006ea696b2df01c074538a1f5
KeyForce <july.master@outlook.com>
2020-05-29 19:31:27 +0800

    New Code

--- Platform/views.py
+++ Platform/views.py
@@ -8,6 +8,7 @@ def home(request):
     try:
         api_requets = requests.get('http://api.github.com/users?since=0')
         api = json.loads(api_requets.content)
+        api_requets.close()
     except requests.exceptions.ConnectionError as e:
         print("连接超时")
         print(e)
@@ -17,17 +18,14 @@ def home(request):
 
 
 def user(request):
-    print(request)
-    if request.method == 'POST':
+    if request.method == 'POST' and request.POST['user']:
         user = request.POST['user']
         import requests
         import json
         api_requets = requests.get('https://api.github.com/users/' + user)
+        api_requets.close()
         username = json.loads(api_requets.content)
         return render(request, 'user.html', {'user': user, 'username': username})
-    elif request.method == 'GET':
-        notfound = '请在搜索框输入你要搜索的内容...'
-        return render(request, 'user.html', {'notfound': notfound})
     else:
         notfound = '请在搜索框输入你要搜索的内容...'
         return render(request, 'user.html', {'notfound': notfound})
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AI_Platform
+
+![image-20200529193112015](image/image-20200529193112015.png)
